b2d708cd3380d795875eca3529777a5b2f55dea8
Alex Stockwell <astockwell@psstudios.com>
2014-01-10 15:13:08 -0700

    added gforms overrides

--- functions.php
+++ functions.php
@@ -6,10 +6,20 @@ foreach (glob(dirname(__FILE__) . '/lib/' . '*.php') as $filename) {
 	require( $filename );
 }
 
-// Define own Jquery file via bower.json
+/**
+ * Move Gravity Forms inline JS into footer after our JS is enqueue'd
+ */
+function init_scripts() {
+	return true;
+}
+add_filter("gform_init_scripts_footer", "init_scripts");
+
+/**
+ * Define own Jquery file via bower.json
+ */
 function flask_scripts_init() {
 	wp_deregister_script( 'jquery' );
-	wp_register_script( 'jquery', get_bloginfo( 'template_directory' ) . '/js/public/script.js', false, null, false);
+	wp_register_script( 'jquery', get_bloginfo( 'template_directory' ) . '/js/public/script.js', false, null, true); // Set last param to false if getting js errors
 	wp_enqueue_script( 'jquery' );
 }
 add_action('wp_enqueue_scripts', 'flask_scripts_init');
