aeba649d0db1cd0e0489934a146c509e549fb3cf
Alex Stockwell <astockwell@gmail.com>
2014-06-24 14:58:28 -0700

    bit the bullet and loaded IE8-friendly (hardcoded) version of jQuery from CDN. Only until Codekits bower lets you specify version/repos

--- functions.php
+++ functions.php
@@ -19,8 +19,9 @@ add_filter("gform_init_scripts_footer", "init_scripts");
  */
 function flask_scripts_init() {
 	wp_deregister_script( 'jquery' );
-	wp_register_script( 'jquery', get_bloginfo( 'template_directory' ) . '/js/dist/libs.min.js', false, null, true); // Set last param to false if getting js errors
+	wp_register_script( 'jquery', '//code.jquery.com/jquery-1.11.1.min.js', false, null, true); // Set last param to false if getting js errors
 	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'app', get_bloginfo( 'template_directory' ) . '/js/dist/app.js', array('jquery') );
+	wp_register_script( 'libs', get_bloginfo( 'template_directory' ) . '/js/dist/libs.min.js', array('jquery'), null, true );
+	wp_enqueue_script( 'app', get_bloginfo( 'template_directory' ) . '/js/dist/app.js', array('jquery', 'libs'), null, true );
 }
 add_action('wp_enqueue_scripts', 'flask_scripts_init');
