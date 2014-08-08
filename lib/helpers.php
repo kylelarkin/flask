89ff30528b165697ed24f9d0873ac3e82feca9e8
Kyle Larkin <kyle@extrasmalldesign.com>
2014-08-08 12:59:02 -0700

    Add Helper for Stripping Caption Widths from WP WYSIWYG

--- lib/helpers.php
+++ lib/helpers.php
@@ -74,3 +74,38 @@ if (!function_exists('get_featured_image')) {
 		return wp_prepare_attachment_for_js( get_post_thumbnail_id( $id ) );
 	}
 }
+
+/**
+ * Remove inline width from WP Image Captions from (http://wp-snippets.com/remove-wp-caption-inline-style-width-in-wordpress-3-4-and-up/)
+ */
+add_shortcode('wp_caption', 'fixed_img_caption_shortcode');
+add_shortcode('caption', 'fixed_img_caption_shortcode');
+function fixed_img_caption_shortcode($attr, $content = null) {
+	// New-style shortcode with the caption inside the shortcode with the link and image tags.
+	if ( ! isset( $attr['caption'] ) ) {
+		if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+			$content = $matches[1];
+			$attr['caption'] = trim( $matches[2] );
+		}
+	}
+
+	// Allow plugins/themes to override the default caption template.
+	$output = apply_filters('img_caption_shortcode', '', $attr, $content);
+	if ( $output != '' )
+		return $output;
+
+	extract(shortcode_atts(array(
+		'id'	=> '',
+		'align'	=> 'alignnone',
+		'width'	=> '',
+		'caption' => ''
+	), $attr));
+
+	if ( 1 > (int) $width || empty($caption) )
+		return $content;
+
+	if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+
+	return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" style="width: ' . $width . 'px">'
+	. do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
